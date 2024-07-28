64e8f6f9f15b9cad044ee59d9f1869788d2c0904
CoC12 <CoC12@users.noreply.github.com>
2024-07-28 09:53:32 +0000

    deploy: 72714f4b3782cb6fd6ff8633d67e14af51cd0699

---
+++ js/components/board.js
@@ -0,0 +1,112 @@
+import { Position } from '../components/position.js';
+import { Element } from '../components/element.js';
+
+
+/**
+ * 盤面を管理するクラス
+ */
+export class Board {
+
+    /**
+     * コンストラクタ
+     * @param {number} cols 列数
+     * @param {number} rows 行数
+     */
+    constructor(cols, rows) {
+        this.cols = cols;
+        this.rows = rows;
+        this.board = Board.createBoard(cols, rows);
+    }
+
+    /**
+     * Element オブジェクトを盤面に設置する。
+     * @param {Position} position Position オブジェクト
+     * @param {Element | null} element Element オブジェクト
+    */
+    setElement(position, element) {
+        this.board[position.y][position.x] = element;
+    }
+
+    /**
+     * Element オブジェクトを取得する。
+     * @param {Position} position Position オブジェクト
+     * @returns {Element | null} Element オブジェクト
+     */
+    getElement(position) {
+        return this.board[position.y][position.x];
+    }
+
+    /**
+     * Element オブジェクトを移動する。
+     * @param {Position} fromPosition 移動元の Position オブジェクト
+     * @param {Position} toPosition 移動先の Position オブジェクト
+     */
+    moveElement(fromPosition, toPosition) {
+        const element = this.getElement(fromPosition);
+        this.setElement(fromPosition, null);
+        this.setElement(toPosition, element);
+    }
+
+    /**
+     * 盤面上の Element オブジェクトを最下部に詰めて移動する。
+     */
+    shiftDown() {
+        for (let x = 0; x < this.cols; x++) {
+            for (let y = this.rows - 1; y >= 0; y--) {
+                const position = new Position(x, y);
+                if (this.isEmpty(position)) {
+                    continue;
+                }
+
+                let newPosition = position;
+                for (let y = 0; y < this.rows - position.y; y++) {
+                    newPosition = newPosition.down;
+                    const isValidMove = this.isValidMove(newPosition);
+                    if (!isValidMove) {
+                        break;
+                    }
+                }
+                this.moveElement(position, newPosition.up);
+            }
+        }
+    }
+
+    /**
+     * 指定した位置が空かどうかを返す。
+     * @param {Position} position Position オブジェクト
+     * @returns {boolean} 空かどうか
+     */
+    isEmpty(position) {
+        return this.getElement(position) === null;
+    }
+
+    /**
+     * 指定した位置が盤面内かどうかを返す。
+     * @param {Position} position Position オブジェクト
+     * @returns {boolean} 盤面内かどうか
+     */
+    isInside(position) {
+        return (
+            (0 <= position.x && position.x < this.cols)
+            && (0 <= position.y && position.y < this.rows)
+        );
+    }
+
+    /**
+     * 指定した位置への移動が可能かを返す。
+     * @param {Position} position Position オブジェクト
+     * @returns {boolean} 移動可能かどうか
+     */
+    isValidMove(position) {
+        return this.isInside(position) && this.isEmpty(position);
+    }
+
+    /**
+     * 盤面を生成する。
+     * @returns {Array<Array<Element | null>>} 盤面
+     */
+    static createBoard(cols, rows) {
+        const board = Array.from({length: rows}, () => Array(cols).fill(null));
+        return board;
+    }
+}
